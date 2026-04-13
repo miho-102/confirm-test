f718dec030ba70ae4d670cb5a47f77dd469593ed
kobayashi miho <s0l.bang1024@icloud.com>
2026-04-13 22:08:25 +0900

    ファイルの追加

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,10 +1,11 @@
-@extends('app.blade.php')
+@extends('layouts.app')
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 
 @section('content')
+
 <div class="contact-form__content">
     <div class="contact-form__heading">
         <h2>Contact</h2>
